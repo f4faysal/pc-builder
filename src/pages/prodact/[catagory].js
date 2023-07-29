422e9462eccd9c9d258700a64c727c2777e144f9
f4faysal <f4faysals@gmail.com>
2023-07-29 19:36:55 +0600

    add a new card for catagory

--- src/pages/prodact/[catagory].js
+++ src/pages/prodact/[catagory].js
@@ -1,13 +1,28 @@
+import Card from "@/component/Card";
 import RootLayout from "@/component/layouts/RootLayout";
 import { useAppSelector } from "@/redux/hook";
 
 const ProdactCatagory = () => {
-     const { products } = useAppSelector((state) => state.products);
+     const { products } = useAppSelector((state) => state.productsCatagory);
      console.log( 'h' ,products)
+
+     const filteredProducts = products.filter((product) => product.category === "RAM");
+console.log(filteredProducts)
      return (
-          <div>
-               ProdactCatagory
+          <div className="container mx-auto my-14">
+          <div className="text-center">
+            <h1 className="text-4xl my-3">Featured Products</h1>
+            <p>Check & Get Your Desired Product!</p>
+  
+  
           </div>
+  
+          <div className="grid  grid-cols-1 md:grid-cols-5 gap-4 justify-items-center my-10">
+            {
+              filteredProducts.map(prodct => <Card prodct={prodct} key={prodct._id} />)
+            }
+          </div>
+        </div>
      );
 };
 
